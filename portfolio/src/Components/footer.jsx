7fe5e6fe04849a3729173a7d18a32292a5356d6a
xBarikadex <71157911+xBarikadex@users.noreply.github.com>
2021-12-10 15:24:46 -0700

    Did some more styling
    Added more colors to the colors file, added extra space to the outsides of the "contact us", and added the Oswald font.

--- portfolio/src/Components/footer.jsx
+++ portfolio/src/Components/footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function footer() {
     return (
         <footer id='footer'>
-            Designed by Barik Boley | 5902 W Peoria Ave, Glendale, AZ 85302 | <a href="mailto:nowhere@gmail.org?cc=name2@rapidtables.com&bcc=name3@reapidtables.com&subject=The%20subject%20of%20the%20email&body=The%20body%20of%20the%email" id="contactUs">Contact Us</a> | Copyright &copy; 2021 My Thoughts
+            Designed by Barik Boley | 5902 W Peoria Ave, Glendale, AZ 85302 | <span className='footer-invisible'>-</span> <a id="contact-us" href="mailto:nowhere@gmail.org?cc=name2@rapidtables.com&bcc=name3@reapidtables.com&subject=The%20subject%20of%20the%20email&body=The%20body%20of%20the%email">Contact Us</a> <span className='footer-invisible'>-</span> | Copyright &copy; 2021 My Thoughts
         </footer>
     )
 }
