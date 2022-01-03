801d01235556c94e7748b5ace05995f94dfb8ca3
xBarikadex <71157911+xBarikadex@users.noreply.github.com>
2022-01-03 15:21:07 -0700

    Worked on the Navabar

--- portfolio/src/Components/navbar.jsx
+++ portfolio/src/Components/navbar.jsx
@@ -3,13 +3,13 @@ import { Link } from 'react-router-dom';
 
 function navbar() {
     return (
-        <nav>
-            <Link to='/'>Barik Boley</Link>
-            <Link to='/'>Homepage</Link>
-            <Link to='/resume'>Resume</Link>
-            <Link to='/aboutMe'>About Me</Link>
-            <Link to='/gallery'>Project Gallery</Link>
-            <Link to='/contact'>Contact Me</Link>
+        <nav id='navbar'>
+            <Link to='/' className='navActive'>Barik Boley</Link>
+            <Link to='/' className='navActive'>Homepage</Link>
+            <Link to='/resume' className='navInactive'>Resume</Link>
+            <Link to='/aboutMe' className='navInactive'>About Me</Link>
+            <Link to='/gallery' className='navInactive'>Project Gallery</Link>
+            <Link to='/contact' className='navInactive'>Contact Me</Link>
         </nav>
     )
 }
