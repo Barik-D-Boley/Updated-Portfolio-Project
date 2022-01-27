1b7fb900e14b187842a9e857be6fae5ee36b2d34
xBarikadex <71157911+xBarikadex@users.noreply.github.com>
2022-01-26 18:18:54 -0700

    Started the Contact Me page

--- src/Pages/ContactMe.jsx
+++ src/Pages/ContactMe.jsx
@@ -6,7 +6,19 @@ function ContactMe() {
     return (
         <div>
             <Navbar active={"contactMe"} />
-            Contact Me
+            <div id="contact-container-div" className="container-div">
+                <h5>Get in Touch</h5>
+                <form action="">
+                    <input type="text" id="email-input" className="email-input" placeholder="Your Email:" /><br />
+                    <input type="text" id="email-subject" className="email-input" placeholder="Subject:" />
+                    <textarea type="text" id="email-content" className="email-input" placeholder="Dear Barik," />
+                    <input type="submit" id="submit-btn" />
+                </form>
+
+                <div id='socials'>
+
+                </div>
+            </div>
             <Footer />
         </div>
     )
