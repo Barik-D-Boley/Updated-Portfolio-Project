c12d4b5b43bea08f7490e61b22e1b8b386f3f286
xBarikadex <bboley269@west-mec.org>
2022-02-06 16:26:28 -0700

    Changed the navbar

--- src/Components/navbar.jsx
+++ src/Components/navbar.jsx
@@ -8,8 +8,10 @@ function Navbar(active) {
     const { active:num } = active;
     return (
         <div id="navbar">
-            <div id="barik"><Link to="/" id="nav-title">Barik Boley</Link></div>
-            <button className={`${(dropDown === true) ? "navbar-toggle-btn-active" : "navbar-toggle-btn"}`} onClick={() => dropDown === false ? setDropDown(true) : setDropDown(false)}><GiHamburgerMenu /></button>
+            <div id="nav-top">
+                <div><Link to="/" id="nav-title">Barik Boley</Link></div>
+                <button className={`${(dropDown === true) ? "navbar-toggle-btn-active" : "navbar-toggle-btn"}`} onClick={() => dropDown === false ? setDropDown(true) : setDropDown(false)}><GiHamburgerMenu /></button>
+            </div>
             <div className={`${(dropDown === true) ? "nav-drop-down" : "nav-pages"}`}>
                 <Link to="/" className={`nav-page-link ${(num === "homepage") ? "nav-active" : "nav-inactive"}`}>Homepage</Link>
                 <Link to="/resume" className={`nav-page-link ${(num === "resume") ? "nav-active" : "nav-inactive"}`}>Resume</Link>
